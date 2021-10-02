b211cf07aa615580e2f9494fec04c317a56efc11
PunitKumar3 <>
2021-10-02 11:41:07 +0000

    Added Welcome Message

---
+++ HelloWorld.java
@@ -0,0 +1,5 @@
+class HelloWorld {
+  public String greetUser(String userName){
+    return "Welcome to the world of Data Structures and Algorithms :" + userName;
+  }
+}
--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("Hello world!");
+    HelloWorld hello = new HelloWorld();
+    String greetings = hello.greetUser("Punit");
+    System.out.println(greetings);
   }
 }
